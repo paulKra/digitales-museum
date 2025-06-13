a8fe9d7dc7bf07ed9177c276a0264ae1c4db68c5
unknown <loades111@gmail.com>
2025-06-13 12:57:18 +0200

    updated astro config and added pnpm version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
-
+  site: "https://paulKra.github.io",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
